34da3592ba13a2618f6b1010979b565f8e478aa7
Garrett Brown <garrettpaulbrown@gmail.com>
2020-09-16 14:01:29 -0700

    Add code to main.js

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import $ from 'jquery';
+import 'bootstrap';
+import './css/styles.css';
+
+
+$(document).ready(function() {
+  $('#calculator-input').submit(function(event) {
+    event.preventDefault();
+    
+  });
+});

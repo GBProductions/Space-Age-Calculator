2c033aa3d64ba53257d2e33b7c585c9c89461fb7
Garrett Brown <garrettpaulbrown@gmail.com>
2020-09-16 14:32:17 -0700

    Write test for Earth age.

---
+++ __tests__/age.test.js
@@ -0,0 +1,9 @@
+import Age from './age.js';
+
+describe('Age', () => {
+
+  test('should return users age in Earth years', () => {
+    const earthInput = new Age(50);
+    expect(this.earthInput).toEqual(50);
+    });
+  })
--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import $ from 'jquery';
 import 'bootstrap';
 import './css/styles.css';
+import Age from './age.js';
 
 
 $(document).ready(function() {
   $('#calculator-input').submit(function(event) {
     event.preventDefault();
-    
+
   });
 });

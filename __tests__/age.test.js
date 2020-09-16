429b10f214ae80240f20bdafdb936447079881a9
Garrett Brown <garrettpaulbrown@gmail.com>
2020-09-16 14:48:39 -0700

    Write mercuryAge test.

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -1,3 +1,4 @@
+import { describe } from 'yargs';
 import Age from './../src/age.js';
 
 describe('Age', () => {
@@ -6,4 +7,14 @@ describe('Age', () => {
     const age = new Age(50);
     expect(age.earthInput).toEqual(50);
     });
-  })   
+  });
+  
+
+describe('mercuryAge', () => {
+
+  test('should return users Earth age in Mercury years', () => {
+  const age = new mercuryAge();
+  expect(age.earthInput).toEqual();
+  expect(age.mercuryAge).toEqual();
+  });
+});
